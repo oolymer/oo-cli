50839f2eed081743e6786f5731c8484978fb0283
Benjamin Gudehus <hastebrot@gmail.com>
2018-03-08 07:55:16 +0100

    (feature) Output element and mixin analysis.

--- index.ts
+++ index.ts
@@ -9,8 +9,16 @@ if (require.main === module) {
 
 export async function main() {
   const analyzer = Analyzer.createForDirectory("test/")
-
   const input = await analyzer.analyzePackage()
   const result = generateAnalysis(input, analyzer.urlResolver)
-  console.log(JSON.stringify(result, null, 2))
+
+  console.log(result.elements!.map(it => [
+    it.name, it.path, it.summary, it.description, it.superclass,
+    it.attributes!.map(it => it.name), it.properties!.map(it => it.name), it.methods!.map(it => it.name)
+  ]))
+  console.log(result.mixins!.map(it => [
+    it.name, it.path, it.summary, it.description, it.superclass,
+    it.attributes!.map(it => it.name), it.properties!.map(it => it.name), it.methods!.map(it => it.name)
+  ]))
+  // console.log(JSON.stringify(result, null, 2))
 }
